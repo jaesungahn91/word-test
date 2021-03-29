b9c8e535df275dbc3bba3376c8440a16d5abba22
jaesungahn91 <jaesungahn91@gmail.com>
2021-03-29 19:26:51 +0900

    init

---
+++ src/test/java/com/js/vocatest/VocaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.vocatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
